ed8e94db236dd2b15a53e98503cae6a2edea6e13
ndeya97 <49243028+ndeya97@users.noreply.github.com>
2020-11-06 12:58:50 +0100

    Uploading my exercices project

---
+++ com.ynov.biblio/src/main/java/com/ynov/bilbio/com/ynov/biblio/App.java
@@ -0,0 +1,13 @@
+package com.ynov.bilbio.com.ynov.biblio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
